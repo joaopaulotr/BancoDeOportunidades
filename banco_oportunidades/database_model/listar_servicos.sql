437298c7ac55cb1ca00975097346b6973f297a3f
Heitor Ferrari <heitorferrari9@gmail.com>
2025-11-02 21:17:49 -0300

    Alteração banco

--- banco_oportunidades/database_model/listar_servicos.sql
+++ banco_oportunidades/database_model/listar_servicos.sql
@@ -1,13 +1,14 @@
--- Mostrar na tela todos os serviços disponíveis com o nome do profissional e a categoria a que pertencem.
+-- Mostrar na tela inicial todos os serviços disponíveis com o nome do profissional e a categoria a que pertencem.
 
 SELECT 
-	s.idServico,
-	s.titulo,
-	s.descricao,
-	s.preco,
-	u.nome AS prestador,
-	c.nomeCategoria AS categoria
- FROM Servicos as s
-INNER JOIN Usuarios as u on s.idUsuario = u.idUsuario
-INNER JOIN Categorias as c on s.idCategoria = c.idCategoria
+    s.idServicos,
+    s.titulo,
+    s.descricao,
+    s.preco,
+    u.nome AS prestador,
+    c.nomeCategoria AS categoria
+FROM Servicos AS s
+INNER JOIN Categorias AS c ON s.Categorias_idCategorias = c.idCategorias
+INNER JOIN Transacoes AS t ON s.idServicos = t.Servicos_idServicos
+INNER JOIN Usuarios AS u ON t.Usuarios_idUsuarios = u.idUsuarios
 WHERE s.status = 'ativo';
--- banco_oportunidades/database_model/modelo.sql
+++ banco_oportunidades/database_model/modelo.sql
@@ -1,59 +1,46 @@
-CREATE SCHEMA BancoOportunidades CHARACTER SET utf8mb4;
+CREATE SCHEMA BancoOportunidades;
 USE BancoOportunidades;
 
 CREATE TABLE Usuarios (
-
-	idUsuario INT AUTO_INCREMENT PRIMARY KEY,
-	nome VARCHAR(100),
-	email VARCHAR(100) UNIQUE,
-	senha_hash VARCHAR(255),
-	cpf_cnpj VARCHAR(20),
-	tipoUsuario ENUM('prestador','cliente','admin'),
-	telefone VARCHAR(20),
-	endereco VARCHAR(255),
-	cidade VARCHAR(100),
-	uf CHAR(2),
+    idUsuarios INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    senha_hash VARCHAR(255),
+    cpf_cnpj VARCHAR(20),
+    tipoUsuario ENUM('prestador','cliente','admin'),
+    telefone VARCHAR(20),
+    endereco VARCHAR(255),
+    cidade VARCHAR(100),
+    uf CHAR(2),
     dataCadastro DATETIME DEFAULT CURRENT_TIMESTAMP
-
 );
 
 CREATE TABLE Categorias (
-
-	idCategoria int not null auto_increment primary key,
-	nomeCategoria varchar (100),
-    descricaoCategoria varchar (255)
-    
+    idCategorias INT AUTO_INCREMENT PRIMARY KEY,
+    nomeCategoria VARCHAR(100),
+    descricaoCategoria VARCHAR(255)
 );
 
 CREATE TABLE Servicos (
-
-  idServico INT AUTO_INCREMENT PRIMARY KEY,
-  idUsuario INT,
-  idCategoria INT,
-  titulo VARCHAR(100),
-  descricao TEXT,
-  preco DECIMAL(10,2),
-  cidade VARCHAR(100),
-  status ENUM('ativo','inativo'),
-  dataCriacao DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (idUsuario) REFERENCES Usuarios(idUsuario),
-  FOREIGN KEY (idCategoria) REFERENCES Categorias(idCategoria)
-  
+    idServicos INT AUTO_INCREMENT PRIMARY KEY,
+    Categorias_idCategorias INT,
+    titulo VARCHAR(100),
+    descricao TEXT,
+    preco DECIMAL(10,2),
+    cidade VARCHAR(100),
+    status ENUM('ativo','inativo'),
+    dataCriacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (Categorias_idCategorias) REFERENCES Categorias(idCategorias)
 );
 
 CREATE TABLE Transacoes (
-
-  idTransacao INT AUTO_INCREMENT PRIMARY KEY,
-  idServico INT,
-  idCliente INT,
-  dataSolicitacao DATETIME DEFAULT CURRENT_TIMESTAMP,
-  valorPago DECIMAL(10,2),
-  status ENUM('pendente','em_andamento','concluido','cancelado'),
-  avaliacao INT,
-  FOREIGN KEY (idServico) REFERENCES Servicos(idServico),
-  FOREIGN KEY (idCliente) REFERENCES Usuarios(idUsuario)
-  
+    idTransacoes INT AUTO_INCREMENT PRIMARY KEY,
+    Usuarios_idUsuarios INT,
+    Servicos_idServicos INT,
+    dataSolicitacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    valorPago DECIMAL(10,2),
+    status ENUM('pendente','em_andamento','concluido','cancelado'),
+    avaliacao INT,
+    FOREIGN KEY (Usuarios_idUsuarios) REFERENCES Usuarios(idUsuarios),
+    FOREIGN KEY (Servicos_idServicos) REFERENCES Servicos(idServicos)
 );
-
-
-
