d7411b5706964f0fbbc58d75398687529bf9c4e1
duudafs <dudascomparin@gmail.com>
2025-11-05 17:20:11 -0300

    icons

--- banco_oportunidades/frontend/src/views/Home.vue
+++ banco_oportunidades/frontend/src/views/Home.vue
@@ -1,54 +1,149 @@
 <script setup>
 import { useRouter } from 'vue-router'
 const router = useRouter()
+import { ref } from "vue"
+
+const track = ref(null)
+
+const cards = [
+  { name: "jardinagem", img: new URL('@/assets/jardinagem.png', import.meta.url).href },
+  { name: "limpeza", img: new URL('@/assets/limpeza.png', import.meta.url).href },
+  { name: "pintura", img: new URL('@/assets/pintura.png', import.meta.url).href },
+  { name: "eletricista", img: new URL('@/assets/eletricista.png', import.meta.url).href },
+  { name: "pedreiro", img: new URL('@/assets/pedreiro.png', import.meta.url).href },
+  { name: "babá", img: new URL('@/assets/baba.png', import.meta.url).href },
+  { name: "cozinheira", img: new URL('@/assets/cozinheira.png', import.meta.url).href }
+]
+
+
+
+const scrollAmount = 260 // largura card + gap
+
+const scrollLeft = () => {
+  track.value.scrollBy({ left: -scrollAmount, behavior: "smooth" })
+}
+
+const scrollRight = () => {
+  track.value.scrollBy({ left: scrollAmount, behavior: "smooth" })
+}
 </script>
 <template>
-  <div class="container text-center">
-  <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
-    <div class="col">
-      <div class="p-3">
-        <div class ="card-jardinagem">
+  <div class="titulo-categoria">Categorias</div>
+  <div class="carousel">
 
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="p-3">Row column</div>
-    </div>
-    <div class="col">
-      <div class="p-3">Row column</div>
-    </div>
-    <div class="col">
-      <div class="p-3">Row column</div>
-    </div>
-    <div class="col">
-      <div class="p-3">Row column</div>
-    </div>
-    <div class="col">
-      <div class="p-3">Row column</div>
-    </div>
-    <div class="col">
-      <div class="p-3">Row column</div>
-    </div>
-    <div class="col">
-      <div class="p-3">Row column</div>
-    </div>
-    <div class="col">
-      <div class="p-3">Row column</div>
-    </div>
-    <div class="col">
-      <div class="p-3">Row column</div>
-    </div>
-  </div>
+    <button class="btn-prev" @click="scrollLeft">‹</button>
+
+    <div class="carousel-track" ref="track">
+     <div class="card" v-for="(item, i) in cards" :key="i">
+  <img :src="item.img" class="card-icon" alt="">
+  <span class="label">{{ item.name }}</span>
 </div>
+    </div>
+
+    <button class="btn-next" @click="scrollRight">›</button>
+  </div>
+
 </template>
 
 <style scoped>
-.card-jardinagem{
-  width: 210px;
-  height: 210px;
+@import url('https://fonts.googleapis.com/css2?family=Coiny&family=Momo+Trust+Display&family=Momo+Trust+Sans:wght@200..800&family=Paytone+One&display=swap');
+
+.carousel {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  width: 100%;
+}
+
+.carousel-track {
+  display: flex;
+  overflow-x: auto;
+  scroll-behavior: smooth;
+  gap: 16px;
+  padding: 20px 0;
+  padding-left: 15px;
+  padding-right: 14px;
+
+}
+
+.carousel-track::-webkit-scrollbar {
+  display: none;
+
+}
+
+.card {
+  min-width: 220px;
+  height: 220px;
+  background: white;
   border-radius: 20px;
-  background-color: rgb(255, 255, 255);
-   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  border: none;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 500;
+  user-select: none;
+  transition: 0.3s;
+}
+
+
+.card:hover {
+  opacity: 0.85;
+  background-color: #d8e2cd;
+  transform: translateY(-2px) scale(1.02);
+}
+
+.btn-prev,
+.btn-next {
+  background: none;
+  border: none;
+  font-size: 40px;
+  color: #555;
+  cursor: pointer;
+  padding: 0 5px;
+  transition: .2s;
+}
+
+.btn-prev:hover,
+.btn-next:hover {
+  color: black;
+}
+
+.titulo-categoria {
+  font-family: "Momo Trust Sans", sans-serif;
+  font-weight: bolder;
+  text-align: start;
+  margin-left: 50px;
+  display: flex;
+  font-size: 18px;
+  width: 125px;
+  background: #f18809;
+  padding: 8px 18px;
+  border: 3px solid #eb5f07;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  border-radius: 999px;
+  /* deixa oval */
+  font-size: 16px;
+  color: #333;
+}
+
+.momo-trust-sans {
+  font-family: "Momo Trust Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+
+}
+
+.card-icon {
+  width: 60px;   /* ajustável */
+  height: 60px;  /* ajustável */
+  object-fit: contain;
+}
+
+.label {
+  font-size: 16px;
+  text-transform: capitalize;
+  font-weight: 600;
+  color: #4c6b42;
 }
 </style>
