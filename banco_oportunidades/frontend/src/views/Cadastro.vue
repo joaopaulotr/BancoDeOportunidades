439265d84d552f75a621813dad89fd0d12e7eeb2
Heitor Ferrari <heitorferrari9@gmail.com>
2025-11-09 19:13:36 -0300

    Estilização

--- banco_oportunidades/frontend/src/views/Cadastro.vue
+++ banco_oportunidades/frontend/src/views/Cadastro.vue
@@ -109,10 +109,12 @@ body.cadastro-page {
   background: linear-gradient(135deg, #aec4d8 0%, #e3e3e9 100%);
   margin: 0;
   padding: 0;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
 }
 
 
@@ -127,10 +129,6 @@ body.cadastro-page {
   -webkit-backdrop-filter: blur(18px);
   box-shadow: 0 15px 50px rgba(0, 0, 0, 0.15);
   transition: 0.3s ease;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
 }
 
 
@@ -140,16 +138,11 @@ body.cadastro-page {
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 40px;
 }
 
 .login-box {
-  width: 100%;
+  width: 75%;
   max-width: 430px;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 }
 
 h1 {
